b83635ac89357927002996cb8bf721dd2a1dc7f2
Heshbon <cityalight.hesbon@gmail.com>
2024-10-17 17:40:03 +0300

    fetches data of chats

--- src/pages/Chat/Chat.jsx
+++ src/pages/Chat/Chat.jsx
@@ -1,19 +1,36 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import './Chat.css'
 import Leftbar from '../../components/Left bar/Leftbar'
 import Rightbar from '../../components/Right bar/Rightbar'
 import ChatBox from '../../components/ChatBox/ChatBox'
+import { AppState } from '../../context/AppState'
 
 const Chat = () => {
+
+  const {chatData, userData} = useContext(AppState);
+  const [fetching, setFetching] = useState(true);
+
+  useEffect(() => {
+    console.log('chatData:', chatData);
+    console.log('userData:', userData);
+    if (chatData.length > 0 && userData) {
+      setFetching(false)
+    }
+  },[chatData,userData])
+
   return (
     <div className='Chat'>
-      <div className="chat-container">
-        <Leftbar />
-        <ChatBox/>
-        <Rightbar />
-      </div>
+      {
+        fetching
+        ?<p className='fetching'>fetching...</p>
+        : <div className="chat-container">
+          <Leftbar />
+          <ChatBox />
+          <Rightbar />
+        </div>
+      }
     </div>
   )
 }
 
-export default Chat
+export default Chat;
