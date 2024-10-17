fc0d867d9b226a28bf86fc82a3f70ec3bb5b7ce5
Heshbon <cityalight.hesbon@gmail.com>
2024-10-17 21:25:00 +0300

    chat data upload updates

--- src/components/ChatBox/ChatBox.jsx
+++ src/components/ChatBox/ChatBox.jsx
@@ -1,7 +1,9 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import './ChatBox.css'
 import assets from '../../assets/assets'
 import { AppState } from '../../context/AppState'
+import { arrayUnion, doc, onSnapshot, updateDoc } from 'firebase/firestore'
+import { db } from '../../config/firebase'
 
 const ChatBox = () => {
 
@@ -9,11 +11,41 @@ const ChatBox = () => {
 
   const [input, setInput] = useState('');
 
+  const postMessage = async () => {
+    try {
+      
+      if (input && threadsId) {
+        await updateDoc(doc(db,'threads', threadsId),{
+          threads: arrayUnion({
+            sId:userData,
+            text:input,
+            createAt:new Data()
+          }
+        })
+        const userIDs = [chatUser.rId,userData.id];
+      }
+    } catch (error) {
+      
+    }
+  }
+
+  useEffect(() => {
+    if (threadsId) {
+      const unSub = onSnapshot(doc(db,'threads',threadsId),(res) =>{
+        setThreads(res.data().threads.reverse())
+        console.log(res.data().threads.reverse());
+      })
+      return () => {
+        unSub();
+      }
+    }
+  },[threadsId])
+
   return chatUser ? (
     <div className='Chat-bar'>
       <div className='Chat-user'>
-        <img src={assets.mary} alt="" />
-        <p>Mary Muthoni <img className='dot' src={assets.bluedot_icon} alt='' /></p>
+        <img src={chatUser.userData.avatar} alt="" />
+        <p>{chatUser.userData.name} <img className='dot' src={assets.bluedot_icon} alt='' /></p>
         <img src={assets.help_icon} className='Help' alt="" />
       </div>
       
@@ -42,7 +74,7 @@ const ChatBox = () => {
       </div>
       <div className="chat-data">
         <input type="text" placeholder='Post a Message' />
-        <input type="file" id='image' accept='image/jpeg, image/png' hidden/>
+        <input onChange={(e) => setInput(e.target.value)} value={input} type="file" id='image' accept='image/jpeg, image/png' hidden/>
         <label htmlFor="image">
           <img src={assets.gallery_icon} alt="" />
         </label>
@@ -56,4 +88,4 @@ const ChatBox = () => {
   </div>
 }
 
-export default ChatBox
+export default ChatBox;
