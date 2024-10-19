282c8b80e234eea8c9b999adcf1f7141a9543cdd
Heshbon <cityalight.hesbon@gmail.com>
2024-10-19 09:44:41 +0300

    prevents unnecessary rendering

--- src/pages/Chat/Chat.jsx
+++ src/pages/Chat/Chat.jsx
@@ -1,9 +1,9 @@
 import React, { useContext, useEffect, useState } from 'react';
 import './Chat.css';
-import Leftbar from '../../components/Left bar/Leftbar'; // Import Leftbar component
-import Rightbar from '../../components/Right bar/Rightbar'; // Import Rightbar component
-import ChatBox from '../../components/ChatBox/ChatBox'; // Import ChatBox component
-import { AppState } from '../../context/AppState'; // Import AppState context
+import Leftbar from '../../components/Left bar/Leftbar';
+import Rightbar from '../../components/Right bar/Rightbar';
+import ChatBox from '../../components/ChatBox/ChatBox';
+import { AppState } from '../../context/AppState';
 
 const Chat = () => {
   // Destructure chatData and userData from AppState context
@@ -13,9 +13,11 @@ const Chat = () => {
   // Effect to check if data has been fetched
   useEffect(() => {
     if (chatData && userData) {
-      setFetching(false); // Stop fetching if data is available
+      setFetching(false);
+      } else {
+        setFetching(true); // Keep fetching if data is not available
       }
-    }, [chatData, userData])
+    }, [chatData, userData]);
     
     return (
     <div className='Chat'>
