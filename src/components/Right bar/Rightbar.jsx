51c15d790e7a2e6346c8cb88819a4d0d4f296ecf
Heshbon <cityalight.hesbon@gmail.com>
2024-10-19 14:44:34 +0300

    post image update

--- src/components/Right bar/Rightbar.jsx
+++ src/components/Right bar/Rightbar.jsx
@@ -7,31 +7,31 @@ import { AppState } from '../../context/AppState';
 const Rightbar = () => {
 
   const { chatUser, threads } = useContext(AppState);
-  const [ threadImages, setThreadImages] = useState([]);
+  const [ postImages, setPostImages] = useState([]);
 
   useEffect (() => {
     let tempData = [];
-    threads.map((thread) => {
-      if (thread.image) {
-        tempData.push(thread.image)
+    threads.map((post) => {
+      if (post.image) {
+        tempData.push(post.image)
       }
     })
-    setThreadImages(tempData);
+    setPostImages(tempData);
   }, [threads])
 
   return chatUser ? (
     <div className='rb'>
       <div className='rb-profile'>
         <img src={chatUser.userData.avatar} alt="" />
-        <h3>{Date.now() - chatUser.userData.lastSeen <= 70000 ?<img className='dot' src={assets.bluedot_icon}alt=''/>:null}{chatUser.userData.name}</h3>
+        <h3>{Date.now() - chatUser.userData.lastSeen <= 80000 ?<img className='dot' src={assets.bluedot_icon}alt=''/>:null}{chatUser.userData.name}</h3>
         <p>{chatUser.userData.info}</p>
       </div>
       <hr/>
       <div className="rb-media">
         <p>Media</p>
         <div>
-        {threadImages.map((url,index)=>(<img onClick={()=>window.open(url)} key={index} src={url} alt="" />))}
-        </div>
+          {postImages.map((url,index)=>(<img onClick={()=>window.open(url)} key={index} src={url} alt="" />))}
+          </div>
         </div>
       <button onClick={()=>logout()}>Logout</button>
     </div>
