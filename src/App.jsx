cc49e2ab6b653b2b537a1aefceae073076cc977c
Heshbon <cityalight.hesbon@gmail.com>
2024-10-19 09:19:08 +0300

    app update

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,40 @@
 import React, { useContext, useEffect } from "react";
 import { Route, Routes, useNavigate } from "react-router-dom";
-import Login from "./pages/Login/Login"; // Importing the Login component
-import Chat from "./pages/Chat/Chat"; // Importing the Chat component
-import Update from "./pages/Profile/Profile"; // Importing the Profile component
-import { ToastContainer } from "react-toastify"; // For toast notifications
-import "react-toastify/dist/ReactToastify.css"; // Importing toast notification styles
-import { onAuthStateChanged } from "firebase/auth"; // For auth state management
-import { auth } from "./config/firebase"; // Importing firebase auth configuration
-import AppStateProvider, { AppState } from "./context/AppState"; // Importing AppState provider and context
+import Login from "./pages/Login/Login"; 
+import Chat from "./pages/Chat/Chat"; 
+import Update from "./pages/Profile/Profile"; 
+import { ToastContainer } from "react-toastify"; 
+import "react-toastify/dist/ReactToastify.css"; 
+import { onAuthStateChanged } from "firebase/auth"; 
+import { auth } from "./config/firebase"; 
+import { AppState } from "./context/AppState"; 
 
 const App = () => {
   const navigate = useNavigate(); // Hook for navigation
-  const { loadUserData, userData } = useContext(AppState); // Accessing state and actions from AppState context
+  const { loadUserData, setChatUser,setThreadsId} = useContext(AppState);
 
   useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, async (user) => {
+    onAuthStateChanged(auth, async (user) => {
       if (user) {
-        await loadUserData(user.uid); // Load user data from Firestore using UID
-
-        // Redirect based on profile completion
-        if (userData && userData.name && userData.avatar) {
-          navigate("/chat"); // Navigate to chat if profile data is complete
+        loadUserData(user.uid); // Load user data from Firestore using UID
         } else {
-          navigate("/profile"); // Navigate to profile if data is incomplete
+          setChatUser(null)
+          setThreadsId(null)
+          navigate('/')
         }
-      } else {
-        navigate("/"); // Navigate to login if user is not authenticated
-      }
-    });
-
-    return () => unsubscribe(); // Cleanup subscription on component unmount
-  }, [navigate, loadUserData, userData]); // Dependencies for useEffect
+      })
+    }, [])
 
   return (
-    <AppStateProvider> // Providing AppState context to the app
+    <>
       <ToastContainer /> // Toast notification container
       <Routes>
-        <Route path="/" element={<Login />} /> // Route for login page
-        <Route path="/chat" element={<Chat />} /> // Route for chat page
-        <Route path="/profile" element={<Update />} /> // Route for profile page
+        <Route path="/chat" element={<Chat />} />
+        <Route path="/" element={<Login />} /> 
+        <Route path="/profile" element={<Update />} />
       </Routes>
-    </AppStateProvider>
-  );
-};
+      </>
+      )
+    };
 
 export default App; // Exporting the App component
